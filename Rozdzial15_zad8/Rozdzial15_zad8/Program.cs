3d71ec702f4a8a324f2364b16bd12aff4e081164
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:12:01 +0100

    Add files via upload

---
+++ Rozdzial15_zad8/Rozdzial15_zad8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rozdzial15_zad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7. Write a program that replaces every occurrence of the substring \"start\" with \"finish\" in a text file." +
+                "Can you rewrite the program to replace whole words only ? Does the program work for large files (e.g. 800 MB)?");
+
+            string filePath = "../../File1.in.txt";
+            string fileContent;
+
+            fileContent = FileToString(filePath);
+            fileContent = Regex.Replace(fileContent, @"(\s|^)start(\s)", "$1finish$2");
+            fileContent = Regex.Replace(fileContent, @"\bstart\b", "finish");
+
+            StreamWriter sw = new StreamWriter("../../File1.out.txt", false);
+
+            sw.Write(fileContent);
+            sw.Close();
+
+            Console.ReadKey();
+        }
+
+        public static string FileToString(string filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            StreamReader sr = new StreamReader(filePath);
+
+            using (sr)
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    sb.Append(line);
+
+                    if (!sr.EndOfStream)
+                    {
+                        sb.Append(Environment.NewLine);
+                    }
+
+                    line = sr.ReadLine();
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ Rozdzial15_zad9/Rozdzial15_zad9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rozdzial15_zad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9. Write a program that deletes all the odd lines of a text file.");
+
+            StreamReader sr = new StreamReader("../../file1.in.txt");
+
+            StringBuilder sb = new StringBuilder();
+
+            using(sr)
+            {
+                int lineCount = 0;
+
+                string oddLineContent = sr.ReadLine();
+
+                while (oddLineContent != null)
+                {
+                    lineCount++;
+
+                    if( lineCount %2 == 0)
+                    {
+                        sb.Append(oddLineContent);
+                        if (!sr.EndOfStream)
+                        {
+                            sb.Append(Environment.NewLine);
+                        }
+                    }
+
+                    oddLineContent = sr.ReadLine();
+                }
+            }
+
+            StreamWriter sw = new StreamWriter("../../file1.out.txt");
+
+            using(sw)
+            {
+                sw.Write(sb.ToString());
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
