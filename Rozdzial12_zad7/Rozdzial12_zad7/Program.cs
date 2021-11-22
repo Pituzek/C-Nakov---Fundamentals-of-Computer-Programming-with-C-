f6cfa465981340654c56e79b75d15c4a57619911
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:08:47 +0100

    Add files via upload

---
+++ Rozdzial12_zad7/Rozdzial12_zad7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Rozdzial12_zad7
+{
+
+    class Program
+    {
+        public static double Pierwiastek(double liczba)
+        {
+            liczba = Math.Sqrt(liczba);
+            return liczba;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7. Write a program that takes a positive integer from the console and prints the square root of this integer. If the input is negative or invalid print \"Invalid Number\" in the console. In all cases print \"Good Bye\".");
+
+            Console.Write("\nEnter number: ");
+            string input = Console.ReadLine();
+            int n = -1;
+            bool invalidNumber = false;
+
+             try
+             {
+                n = Convert.ToInt32(input);
+             }
+             catch (FormatException e)
+             { 
+                Console.WriteLine("Invalid number!");
+                invalidNumber = true;
+             }
+             finally
+             {
+                if (n < 0)
+                {
+                    if (!invalidNumber) Console.WriteLine("Invalid number!");
+                }
+                else Console.WriteLine(Math.Sqrt(n));    
+             }
+
+            Console.WriteLine("Good Bye");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial12_zad8/Rozdzial12_zad8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Rozdzial12_zad8
+{
+    class Program
+    {
+        public static void Zakres(int x, int y)
+        {
+            int number;
+            Console.WriteLine("\nPodaj liczbe z zakresu start-end");
+            number = int.Parse(Console.ReadLine());
+
+            if (number <= x || number <= y)
+            {
+                for (int i = number; i <= number + 10; i++)
+                {
+                    Console.Write("\n{0} ,", i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nPodano liczbe spoza zakresu");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("8. Write a method ReadNumber(int start, int end) that reads an integer from the console in the range [start…end]. In case the input integer is not valid or it is not in the required range throw appropriate exception. Using this method, write a program that takes 10 integers a1, a2, …, a10 such that 1 < a1 < … < a10 < 100.");
+
+            Console.WriteLine("Podaj zakres od - do (1 < a < 100)");
+            Console.WriteLine("Start:");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("End:");
+            int end = int.Parse(Console.ReadLine());
+
+            if (end < start + 9)
+                Console.WriteLine("\nPodano zly zakres");
+            else
+                Zakres(start, end);
+
+            Console.ReadKey();
+        }
+    }
+}
