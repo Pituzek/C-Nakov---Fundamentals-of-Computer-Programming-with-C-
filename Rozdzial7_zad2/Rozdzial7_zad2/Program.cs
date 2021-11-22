e0ee3d50ca001ea32ea24190a20f0f3cdd79a463
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:57:15 +0100

    Add files via upload

---
+++ Rozdzial7_zad2/Rozdzial7_zad2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Rozdzial7_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. Write a program, which reads two arrays from the console and checks whether they are equal (two arrays are equal when they are of equal length and all of their elements, which have the same index, are equal).");
+
+            Console.WriteLine("\nPodaj wielkosc pierwszej tablicy");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPodaj wielkosc drugiej tablicy");
+            int m = int.Parse(Console.ReadLine());
+
+            int[] tablica1 = new int[n];
+            int[] tablica2 = new int[m];
+
+            bool rowna = true;
+
+            if (tablica1.Length != tablica2.Length) rowna = false;
+
+            if (rowna)
+            {
+                Console.WriteLine("\nWpisz wartosci pierwszej tablicy");
+
+                for( int i = 0; i < tablica1.Length; i++)
+                {
+                    tablica1[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("\nWpisz wartosci drugiej tablicy");
+
+                for (int i = 0; i < tablica2.Length; i++)
+                {
+                    tablica2[i] = int.Parse(Console.ReadLine());
+                }
+
+                for(int i=0;i<tablica1.Length;i++)
+                {
+                    if (tablica1[i] != tablica2[i]) rowna = false;
+                }
+
+            }
+
+            Console.WriteLine("\nTablice sa rowne? {0}", rowna);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial7_zad3/Rozdzial7_zad3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Rozdzial7_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. Write a program, which compares two arrays of type char lexicographically (character by character) and checks, which one is first in the lexicographical order.");
+
+            bool arrayEqual = true;
+            char[] arrA = new char[5] { 'a', 'b', 'c', 'd', 'e' };
+            char[] arrB = new char[5] { 'a', 'b', 'c', 'd', 'e' };
+
+            if (arrA.Length > arrB.Length) Console.WriteLine("\nSecond array is lexicographicaly first.");
+            else if (arrA.Length < arrB.Length) Console.WriteLine("\nFirst array is lexicographicaly first.");
+            else
+            {
+                for (int i = 0; i < arrA.Length; i++)
+                {
+                    if (arrA[i] < arrB[i])
+                    {
+                        Console.WriteLine("\nFirst array is lexicographicaly first.");
+                        arrayEqual = false;
+                        break;
+                    }
+                    if (arrA[i] > arrB[i])
+                    {
+                        Console.WriteLine("\nSecond array is lexicographicaly first.");
+                        arrayEqual = false;
+                        break;
+                    }
+                }
+
+                if (arrayEqual) Console.WriteLine("\nArrays are lexicographicaly equal.");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
