31ca105ad6560604ad385469f91ed54799501ea6
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:45:17 +0100

    Add files via upload

---
+++ Rozdzial4_zad8/Rozdzial4_zad8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rozdzial4_zad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 8.Write a program that reads five numbers from the console and prints the greatest of them.
+             */
+
+            Console.WriteLine("Ile liczb chcesz wpisac do konsoli?");
+            int ile = int.Parse(Console.ReadLine());
+
+            najwieksza(ile);
+        }
+
+        static void najwieksza(double liczby)
+        {
+
+            double najw = 0;
+            double[] tab = new double[(int)(liczby)];
+
+            for (int i=0; i<liczby;i++)
+            {
+                Console.WriteLine("Podaj liczbe nr {0}",i);
+
+                tab[i] = double.Parse(Console.ReadLine());
+
+                najw = tab[i] > najw ? tab[i] : najw;
+
+            }
+
+            Console.WriteLine("Najwieksza liczba z podanych to: {0}", najw);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Rozdzial4_zad9/Rozdzial4_zad9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Rozdzial4_zad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             *  9. Write a program that reads an integer number n from the console. After that reads n numbers from the console and prints their sum.
+             */
+
+
+            Console.WriteLine("Ile liczb n, chciałbyś zsumować?");
+            int ile = int.Parse(Console.ReadLine());
+
+            double ileTab = ile;
+
+            double[] tab = new double[(int)ileTab];
+
+            
+
+            for (int i = 0; i < ile; i++)
+            {
+                Console.WriteLine("Podaj liczbe nr {0}", i);
+                tab[i] = double.Parse(Console.ReadLine());
+            }
+
+            double suma = 0;
+
+            foreach (double l in tab)
+            {
+                
+                suma += l;
+            }
+
+            Console.WriteLine("Suma podanych liczb wynosi: {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
