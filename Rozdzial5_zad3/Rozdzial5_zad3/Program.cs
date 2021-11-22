aaed4a40365fd7310137ee3f2101dddc3ba6be5a
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:47:32 +0100

    Add files via upload

---
+++ Rozdzial5_zad3/Rozdzial5_zad3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Rozdzial5_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*3. Write a program that finds the biggest of three integers, 
+             * using nested if statements. */
+
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if(a>b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("{0} is biggest", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is biggest", c);
+                }
+            }
+            else
+            {
+                if (b>c)
+                {
+                    Console.WriteLine("{0} is biggest", b);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is biggest", c);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial5_zad4/Rozdzial5_zad4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Rozdzial5_zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 4. Sort 3 real numbers in descending order. Use nested if statements.
+             */
+            //https://github.com/ivanpop/CS-for-Dummies/blob/master/Chapter%205%20Solution%204/Program.cs
+
+            Console.WriteLine("Podaj liczbe a");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe b");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe c");
+            int c = int.Parse(Console.ReadLine());
+
+
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+
+                    if (b > c) // DLA 4 5 6 NIE DZIALA USPRAWNIC
+                    {
+                        a = a + b;
+                        b = a - b;
+                        a = a - b;
+                    }
+                }
+                else if (a >= c)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+            }
+            else if (a == b)
+            {
+                if (a < c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    b = b + c;
+                    c = b - c;
+                    b = b - c;
+                }
+                if (a < b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+            }
+            Console.WriteLine("{0}, {1}, {2}", a, b, c);
+            Console.ReadLine();
+
+
+        }
+    }
+}
