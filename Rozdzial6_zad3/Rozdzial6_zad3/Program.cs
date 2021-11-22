98ed2cf268a08529279dcf783671b13970c9e4f3
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:52:44 +0100

    Add files via upload

---
+++ Rozdzial6_zad3/Rozdzial6_zad3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rozdzial6_zad3
+{
+    class Program
+    {
+        /*
+         * 3. Write a program that reads from the console a series of integers and prints
+         * the smallest and largest of them.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj zakres liczb N");
+            int zakres = int.Parse(Console.ReadLine());
+
+            int min = 0;
+            int max = 0;
+
+            for (int i=0;i<zakres;i++)
+            {
+                Console.WriteLine("Podaj liczbe");
+                int liczba = int.Parse(Console.ReadLine());
+
+                if (i == 0) min = max = liczba;
+
+                if (min > liczba) min = liczba;
+                if (max < liczba) max = liczba;
+
+
+            }
+            Console.WriteLine("Max {0} ; Min {1}", max, min);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial6_zad5/Rozdzial6_zad5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rozdzial6_zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Write a program that reads from the console number N and print the sum of the first N members of the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8,13, 21, 34, 55, 89, 144, 233, 377, …");
+
+            Console.WriteLine("Podaj liczbe N");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("0, 1,");
+
+            int pierwszyN=0, trzeciN=0, drugiN=1;
+
+            for(int i=2; i<n; i++)
+            {
+                trzeciN = pierwszyN + drugiN;
+                Console.Write(" {0},", trzeciN);
+                pierwszyN = drugiN;
+                drugiN = trzeciN;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
