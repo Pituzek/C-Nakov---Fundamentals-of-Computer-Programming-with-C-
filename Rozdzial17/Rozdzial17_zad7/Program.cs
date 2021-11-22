f85a035dbd770864357f3643b830db2e873c1a3d
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:19:59 +0100

    Add files via upload

---
+++ Rozdzial17/Rozdzial17_zad7/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rozdzial17_zad7
+{
+    public class Graph
+    {
+        private List<int>[] childNodes;
+
+        public Graph(int size)
+        {
+            this.childNodes = new List<int>[size];
+            for (int i = 0; i < size; i++)
+            {
+                this.childNodes[i] = new List<int>();
+            }
+
+        }
+
+        public Graph(List<int>[] childNodes)
+        {
+            this.childNodes = childNodes;
+        }
+
+        public int Size
+        {
+            get { return this.childNodes.Length; }
+        }
+
+        public void AddEdge(int u, int v)
+        {
+            childNodes[u].Add(v);
+        }
+
+        public void RemoveEdge(int u, int v)
+        {
+            childNodes[u].Remove(v);
+        }
+
+        public bool HasEdge(int u, int v)
+        {
+            bool hasEdge = childNodes[u].Contains(v);
+            return hasEdge;
+        }
+
+        public IList<int> GetSuccessors(int v)
+        {
+            return childNodes[v];
+        }
+
+        public void SearchBFS(Graph graph, int u, int v)
+        {
+            Queue<IList<int>> queue = new Queue<IList<int>>();
+
+            bool[] visited = new bool[graph.Size];
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                visited[i] = false;
+            }
+
+            queue.Enqueue(graph.GetSuccessors(u));
+
+            visited[u] = true;
+
+            while ( queue.Count > 0)
+            {
+
+            }
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7.Let’s have as given a graph G(V, E) and two of its vertices x and y.
+            // Write a program that finds the shortest path between two vertices measured in number of vertices staying on the path.
+
+
+            Graph graph = new Graph(new List<int>[]
+            {
+                new List<int> {4},          // successors of vertice 0
+                new List<int> {1, 2, 6},    // successors of vertice 1
+                new List<int> {1, 6},       // successors of vertice 2
+                new List<int> {6},          // successors of vertice 3
+                new List<int> {0},          // successors of vertice 4
+                new List<int> {},           // successors of vertice 5
+                new List<int> {1, 2, 3}     // successors of vertice 6
+
+            });
+
+            
+
+            graph.SearchBFS(graph, 1, 6);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial17/Rozdzial17_zad7_v2/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rozdzial17_zad7_v2
+{
+
+    public class Graph
+    {
+        int[,] nodes;
+        bool[] visited;
+        int[] currentStepsLength;
+        int[] currentPath;
+
+        public Graph(int[,] nodes)
+        {
+            this.nodes = nodes;
+            this.visited = new bool[nodes.GetLength(0)];
+            this.currentStepsLength = new int[nodes.GetLength(0)];
+
+            for (int i = 0; i < currentStepsLength.GetLength(0); i++)
+            {
+                currentStepsLength[i] = int.MaxValue;
+            }
+
+            currentPath = new int[nodes.GetLength(0)];
+        }
+
+        private int GetPreviousNode(int currentNode)
+        {
+            return currentPath[currentNode];
+        }
+
+        public Stack<int> GetPath(int start, int end)
+        {
+            FindShortestPath(start, end);
+            Stack<int> path = new Stack<int>();
+
+            int current = end;
+
+            do
+            {
+                path.Push(current);
+                current = GetPreviousNode(current);
+
+            } while (current != start);
+
+            path.Push(current);
+
+            return path;
+        }
+
+        private void FindShortestPath(int start, int end)
+        {
+            Dictionary<int, int> notVisited = new Dictionary<int, int>();
+
+            for (int i = 0; i < visited.GetLength(0); i++)
+            {
+                notVisited.Add(i, i);
+            }
+
+            int currentNode = start;
+
+            while (notVisited.Count > 0)
+            {
+                List<int> connectedNodes = GetConnectedNodes(currentNode);
+
+                foreach(int node in connectedNodes)
+                {
+                    if (!visited[node])
+                    {
+                        if (currentStepsLength[currentNode] < int.MaxValue)
+                        {
+                            int newLength = currentStepsLength[currentNode] + nodes[currentNode, node];
+
+                            if (newLength < currentStepsLength[node])
+                            {
+                                currentStepsLength[node] = currentStepsLength[currentNode] + nodes[currentNode, node];
+                                currentPath[node] = currentNode;
+                            }
+                        }
+                        else
+                        {
+                            currentStepsLength[node] = nodes[currentNode, node];
+                            currentPath[node] = currentNode;
+                        }
+                    }
+                }
+
+                visited[currentNode] = true;
+
+                if (currentNode == end)
+                {
+                    return;
+                }
+
+                notVisited.Remove(currentNode);
+
+                int currentMinValue = int.MaxValue;
+                int currentMinIndex = -1;
+
+                for (int i = 0; i < currentStepsLength.GetLength(0); i++)
+                {
+                    if (!visited[i] && currentMinValue > currentStepsLength[i])
+                    {
+                        currentMinValue = currentStepsLength[i];
+                        currentMinIndex = i;
+                    }
+                }
+
+                if (currentMinValue == int.MaxValue)
+                {
+                    return;
+                }
+
+                currentNode = currentMinIndex;
+
+            }
+        }
+
+        private List<int> GetConnectedNodes(int node)
+        {
+            List<int> lst = new List<int>();
+
+            for (int i = 0; i < nodes.GetLength(0); i++)
+            {
+                if (nodes[node, i] > 0)
+                {
+                    lst.Add(i);
+                }
+            }
+
+            return lst;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * 
+             * 10
+0, 1, 0, 0, 0, 0, 0, 1, 0, 0
+0, 0, 0, 1, 0, 0, 1, 0, 0, 0
+0, 0, 0, 0, 0, 0, 0, 1, 0, 0
+0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 1, 0, 0, 0, 0, 0, 0, 1
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+             * 0, 2
+             * 
+             */
+
+            Graph g = new Graph(ReadGraph());
+
+            string input = Console.ReadLine();
+            string[] searchedPath = input.Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<int> path = g.GetPath(int.Parse(searchedPath[0]), int.Parse(searchedPath[1]));
+
+            PrintResult(path);
+        }
+
+        static int[,] ReadGraph()
+        {
+            string input = Console.ReadLine();
+            int N = int.Parse(input);
+
+            int[,] graph = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                input = Console.ReadLine();
+                string[] rowElements = input.Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < N; j++)
+                {
+                    graph[i, j] = int.Parse(rowElements[j]);
+                }
+            }
+
+            return graph;
+        }
+
+        static void PrintResult(Stack<int> path)
+        {
+            StringBuilder builder = new StringBuilder(path.Count * 2);
+
+            while (path.Count > 0)
+            {
+                if (path.Count == 1)
+                {
+                    builder.Append(path.Pop());
+                }
+                else
+                {
+                    builder.Append(String.Format("{0} ", path.Pop()));
+                }
+            }
+
+            Console.WriteLine(builder);
+        }
+    }
+}
