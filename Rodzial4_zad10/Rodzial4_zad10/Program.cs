c37db57eb6ae881c9de32d6382be56048bb1e25a
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:42:46 +0100

    Add files via upload

---
+++ Rodzial4_zad10/Rodzial4_zad10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rodzial4_zad10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           10. Write a program that reads an integer number n from the console and prints all 
+               numbers in the range[1…n], each on a separate line.
+            */
+            
+            Console.WriteLine("Podaj liczbe całkowitą n");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            for (int i=1; i<=n; i++)
+            {
+                int l =+ i;
+                Console.WriteLine(l);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Rodzial4_zad5/Rodzial4_zad5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Rodzial4_zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         		Console.WriteLine("Podaj liczbe a");
+				int a = int.Parse(Console.ReadLine());
+				//int a = 14;
+
+				Console.WriteLine("Podaj liczbe b");
+				int b = int.Parse(Console.ReadLine());
+				//int b = 25;
+
+
+				int liczba = (b>a ? b-a : (a-b));
+
+				int licznik = 0;
+
+				int[] tablica = new int[liczba];
+
+				tablica[0] = a;
+
+				for (int i = 0; i < tablica.Length; i++)
+				{
+					if (i == 0)
+					{
+						tablica[i] += 1;
+					}
+					else
+						tablica[i] = tablica[i - 1] + 1;
+
+					Console.WriteLine(tablica[i]);
+				}
+
+				foreach (int i in tablica)
+				{
+
+					if (i % 5 == 0)
+					{
+						licznik++;
+					}
+
+
+				}
+
+			Console.WriteLine("{0}", licznik);
+
+			Console.ReadKey();
+
+			}
+		
+		
+
+	}
+    
+}
