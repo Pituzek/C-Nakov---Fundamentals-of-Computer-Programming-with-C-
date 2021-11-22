b9051bc18e513feb385bab3468b409f213056b35
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:08:46 +0100

    Add files via upload

---
+++ Rozdzial12_zad10/Rozdzial12_zad10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Rozdzial12_zad10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10. Write a method that takes as a parameter the name of a binary file, reads the content of the file and returns it as an array of bytes. Write a method that writes the file content to another file. Compare both files.");
+
+            var fileName = @"C:\Users\piotr\Desktop\test.txt";
+            var fileNameCopy = @"C:\Users\piotr\Desktop\testCopy.txt";
+
+            //using (MemoryStream ms = new MemoryStream()) ;
+            using FileStream fs = File.OpenRead(fileName);
+            
+            //byte[] buf = new byte[1024];
+           
+            
+            byte[] buff = File.ReadAllBytes(fileName);
+            File.WriteAllBytes(fileNameCopy, buff);
+
+
+            /*
+            int c;
+
+            using (MemoryStream ms = new MemoryStream())
+            using (FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                // byte[] bytes = new byte[file.Length];
+                //file.Read(bytes, 0, (int)file.Length);
+                //ms.Write(bytes, 0, (int)file.Length);
+                while ((c = file.Read(buf, 0, buf.Length)) > 0)
+                {
+                    // Console.WriteLine(Encoding.UTF8.GetString(buf, 0, c));
+                    file.Read(buf, 0, buf.Length);
+                    ms.Write(buf, 0, buf.Length);
+
+                }
+            }
+
+
+            /Read file to byte array
+
+FileStream stream = File.OpenRead(@"c:\path\to\your\file\here.txt");
+byte[] fileBytes= new byte[stream.Length];
+
+stream.Read(fileBytes, 0, fileBytes.Length);
+stream.Close();
+//Begins the process of writing the byte array back to a file
+
+using (Stream file = File.OpenWrite(@"c:\path\to\your\file\here.txt"))
+{
+   file.Write(fileBytes, 0, fileBytes.Length);
+}
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial12_zad9/Rozdzial12_zad9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Rozdzial12_zad9
+{
+    class Program
+    {
+        public static void ReadFile(string theSamefileName)
+        {
+            StreamReader reader = new StreamReader(theSamefileName);
+            int lineNumber = 0;
+            // Read first line from the text file
+            string line = reader.ReadLine();
+            // Read the other lines from the text file
+            while (line != null)
+            {
+                lineNumber++;
+                Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                line = reader.ReadLine();
+            }
+            // Close the resource after you've finished using it
+            reader.Close();
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("9. Write a method that takes as a parameter the name of a text file, reads the file and returns its content as string. What should the method do if and exception is thrown?");
+            
+            //string fileName = "C:\\Users\\piotr\\Desktop\\test.txt";
+            string theSamefileName = @"C:\Users\piotr\Desktop\test.txt";
+
+            try
+            {
+                ReadFile(theSamefileName);
+            }
+            catch(FileNotFoundException fefe)
+            {
+                Console.WriteLine("Plik {0}, nie został znaleziony", theSamefileName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
