744f78de22a8fd53bde5e8e663732bf3394172ef
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:43:37 +0100

    Add files via upload

---
+++ Rozdzial4_zad2/Rozdzial4_zad2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Rozdzial4_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj promień R");
+                
+            double _r = double.Parse(Console.ReadLine());
+
+            double pi = Math.PI;
+
+            Console.WriteLine("Obwod kola wynosi: {0}", Obwod(_r, pi));
+            Console.WriteLine("Pole kola wynosi: {0}", Pole(_r, pi));
+
+        }
+
+        static double Obwod(double promien, double _pi)
+        {
+            double obwod = 2 * _pi * promien;
+            return obwod;
+        }
+
+        static double Pole(double promien, double _pi)
+        {
+            double pole = _pi * (Math.Pow(promien,2));
+            return pole;
+        }
+
+
+    }
+}
---
+++ Rozdzial4_zad3/Rozdzial4_zad3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Rozdzial4_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* A given company has name, address, phone number, fax number, web site and manager. 
+            * The manager has name, surname and phone number.Write a program that reads information 
+            * about the company and its manager and then prints it on the console.                               
+            */
+
+            Console.WriteLine("Podaj nazwe firmy");
+                string nazwa = Console.ReadLine();
+            Console.WriteLine("Podaj adres firmy");
+                string adres = Console.ReadLine();
+            Console.WriteLine("Podaj numer tel do firmy");
+                string numerTelFirmy = Console.ReadLine();
+            Console.WriteLine("Podaj numer fax do firmy");
+                string numerFaxFirmy = Console.ReadLine();
+            Console.WriteLine("Podaj adres www firmy");
+                string stronaWWW = Console.ReadLine();
+            Console.WriteLine("Podaj imie menadzera");
+                string imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko menadzera");
+                string nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj tel menadzera");
+                string telMenadzera = Console.ReadLine();
+
+            Console.WriteLine("Nazwa firmy: {0:10} \n" + "Adres firmy: {1:10} \n" + 
+                              "Tel: {2:10} \n" + "Fax: {3:10} \n" + "www: {4:10} \n" + "\n" +
+                              "Imie: {5:10} \n" + "Nazwisko: {6:10} \n" + "Tel: {7:10}"
+                              ,nazwa,adres,numerTelFirmy,numerFaxFirmy ,stronaWWW ,imie ,nazwisko ,telMenadzera );
+
+            Console.ReadKey();
+        }
+    }
+}
