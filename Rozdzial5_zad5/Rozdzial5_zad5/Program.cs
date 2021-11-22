1ba49deaf8726132b0eca83f1c077ddaccfaf993
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:48:55 +0100

    Add files via upload

---
+++ Rozdzial5_zad5/Rozdzial5_zad5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Rozdzial5_zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 5. Write a program that asks for a digit (0-9), and depending on the input, 
+             * shows the digit as a word (in English). Use a switch statement.
+             */
+
+            Console.WriteLine("Podaj liczbe z zakresu 0-9");
+            int liczba = int.Parse(Console.ReadLine());
+
+            switch(liczba)
+            {
+                case 1:
+                    Console.WriteLine("jeden");
+                    break;
+                case 2:
+                    Console.WriteLine("dwa");
+                    break;
+                case 3:
+                    Console.WriteLine("trzy");
+                    break;
+                case 4:
+                    Console.WriteLine("cztery");
+                    break;
+                case 5:
+                    Console.WriteLine("piec");
+                    break;
+                case 6:
+                    Console.WriteLine("szesc");
+                    break;
+                case 7:
+                    Console.WriteLine("siedem");
+                    break;
+                case 8:
+                    Console.WriteLine("osiem");
+                    break;
+                case 9:
+                    Console.WriteLine("dziewiec");
+                    break;
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+                default:
+                    Console.WriteLine("Podano liczbe spoza zakresu");
+                    break;
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rozdzial5_zad6/Rozdzial5_zad6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Rozdzial5_zad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 6. Write a program that gets the coefficients a, b and c of a quadratic equation:
+             * ax2 + bx + c, calculates and prints its real roots (if they exist). 
+             * Quadratic equations may have 0, 1 or 2 real roots.
+             */
+
+            Console.WriteLine("Podaj a");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj c");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = Math.Pow(b,2) - (4 * a * c);
+
+            double x1 = 0, x2 = 0;
+
+            if (delta > 0)
+            {
+                x1 = ((b * -1) - Math.Sqrt(delta)) / (2 * a);
+                x2 = ((b * -1) + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("x1 = {0}", x1);
+                Console.WriteLine("x2 = {0}", x2);
+            }
+            else if (delta == 0)
+            {
+                x1 = (b * -1) / (2 * a);
+                Console.WriteLine("x1 = {0}", x1);
+            }
+            else
+            {
+                Console.WriteLine("Delta = {0}, brak rozwiazan", delta);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
