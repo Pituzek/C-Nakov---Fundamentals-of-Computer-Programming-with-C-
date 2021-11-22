0cce93ae4f4022c7f4f9be5f0c6e1f5e4a4259a1
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:46:01 +0100

    Add files via upload

---
+++ Rozdzial4_zad11/Rozdzial4_zad11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Rozdzial4_zad11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+                11. Write a program that prints on the console the first 100 numbers 
+                    in the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, …
+             * 
+             */
+
+
+            int fibo = 100;
+
+            double a = 0;
+            double b = 1;
+            double c = 0;
+
+            Console.Write(" {0} + {1}",a,b);
+
+            for (int i=2;i<fibo;i++)
+            {
+                c = a + b;
+                Console.Write(" {0}",c);
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Rozdzial4_zad12/Rozdzial4_zad12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rozdzial4_zad12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             *
+                12. Write a program that calculates the sum (with precision of 0.001) 
+                    of the following sequence: 1 + 1/2 - 1/3 + 1/4 - 1/5 + …
+             *
+             */
+
+            Console.Write("Enter last number: ");
+            int length = Int32.Parse(Console.ReadLine());
+            double sum = 1.0;
+
+            for (int i = 2; i <= length; i++)
+            {
+                sum += (1.0 / i);
+            }
+
+            Console.WriteLine("{0:F3}", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
