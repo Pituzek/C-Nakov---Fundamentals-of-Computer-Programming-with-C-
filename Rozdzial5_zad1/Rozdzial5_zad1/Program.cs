c9340c0703ea3cf5d218bff9c8c4430de3a33dd0
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:46:50 +0100

    Add files via upload

---
+++ Rozdzial5_zad1/Rozdzial5_zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Rozdzial5_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Write an if-statement that takes two integer variables and exchanges 
+             * their values if the first one is greater than the second one. 
+             */
+
+
+            Console.WriteLine("Podaj liczbe a");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe b");
+            double b = double.Parse(Console.ReadLine());
+
+            double tmp;
+
+            if (a>b)
+            {
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            Console.WriteLine("a:{0} b:{1}", a, b);
+
+
+            Console.ReadKey();
+
+
+           
+        }
+    }
+}
---
+++ Rozdzial5_zad2/Rozdzial5_zad2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Rozdzial5_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * 2. Write a program that shows the sign (+ or -) of the product of three real numbers,
+             * without calculating it. Use a sequence of if operators.
+             * 
+             */
+
+            int negativeNumbersCount = 0;
+
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            if (a < 0)
+            {
+                negativeNumbersCount += 1;
+            }
+
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            if (b < 0)
+            {
+                negativeNumbersCount += 1;
+            }
+
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (c < 0)
+            {
+                negativeNumbersCount += 1;
+            }
+
+
+            if (a == 0 || b==0 || c == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (negativeNumbersCount % 2 == 0)
+                {
+                    Console.WriteLine("+");
+                }
+                else if (negativeNumbersCount % 2 != 0)
+                {
+                    Console.WriteLine("-");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
