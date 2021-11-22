2e3fd5f6300062d3a716fcaffdd1a22fc30c0755
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:05:38 +0100

    Add files via upload

---
+++ Rozdzial10_zad5/Rozdzial10_zad5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Rozdzial10_zad5
+{
+    class Program
+    {
+        static string[] strings, str;
+        static int length;
+
+        static void cycle(int iter, int index, int k)
+        {
+            if (iter == k)
+            {
+                for (int i = 0; i < length; i++)
+                    Console.WriteLine("({0})", str[i]);
+                return;
+            }
+
+            for (int i = index; i < strings.Length; i++)
+            {
+                str[iter] = strings[i];
+                cycle(iter + 1, i + 1, k);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a recursive program, which prints all subsets of a given set of N words. Example input:");
+            /*
+             * 
+             words = {'test', 'rock', 'fun'}
+             Example output:
+             (), (test), (rock), (fun), (test rock), (test fun),
+             (rock fun), (test rock fun)
+             Think about and implement an iterative algorithm for the same task.
+             * 
+             */
+
+            Console.Write("Enter words length: ");
+            length = Int32.Parse(Console.ReadLine());
+
+            strings = new string[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} word: ", i + 1);
+                strings[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i <= length; i++)
+            {
+                str = new string[length];
+                cycle(0, 0, i);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial10_zad7/Rozdzial10_zad7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Rozdzial10_zad7
+{
+    class Program
+    {
+        static void FindCombinations(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                Console.Write("{");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write("}, ");
+            }
+            else
+            {
+                for (int i = index; i < arr.Length; i++)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[index];
+                    arr[index] = temp;
+                    FindCombinations(arr, index + 1);
+                    temp = arr[i];
+                    arr[i] = arr[index];
+                    arr[index] = temp;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7. Write a recursive program, which generates and prints all permutations of the numbers 1, 2, …, n, for a given integer n. Example input:");
+            
+            /*
+             * 
+             n = 3
+             Example output:
+             (1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)
+             Try to find an iterative solution for generating permutations.
+             * 
+             */
+
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++) arr[i] = i + 1;
+            FindCombinations(arr, 0);
+
+            Console.ReadKey();
+        }
+    }
+}
