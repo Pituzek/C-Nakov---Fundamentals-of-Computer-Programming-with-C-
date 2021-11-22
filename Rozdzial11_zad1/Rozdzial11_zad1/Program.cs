98174fa357fefc224292e646728ad57c36ee523a
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:05:50 +0100

    Add files via upload

---
+++ Rozdzial11_zad1/Rozdzial11_zad1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Rozdzial11_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Write a program, which reads from the console a year and checks if it is a leap year.");
+
+            Console.WriteLine("\nPodaj rok");
+
+            int rok = int.Parse(Console.ReadLine());
+
+            bool przestepny = DateTime.IsLeapYear(rok);
+
+            if(przestepny == true) Console.WriteLine("\nRok {0}, jest rokiem przestępnym", rok);
+            else Console.WriteLine("\nRok {0}, nie jest rokiem przestępnym", rok);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial11_zad2/Rozdzial11_zad2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Rozdzial11_zad2
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        static void PrintRandom(int[] tablica)
+        {
+
+            foreach(int liczba in tablica)
+            {
+                Console.Write("{0} ,",liczba);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. Write a program, which generates and prints on the console 10 random numbers in the range [100, 200].");
+
+            int[] tab = new int[10];
+
+            for(int i=0; i<10; i++)
+            {
+                tab[i] = rnd.Next(100, 201);
+            }
+
+            PrintRandom(tab);
+
+            Console.ReadKey();
+        }
+    }
+}
