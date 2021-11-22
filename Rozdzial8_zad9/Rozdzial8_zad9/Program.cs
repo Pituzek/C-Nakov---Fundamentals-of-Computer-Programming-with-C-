7d943d92eb45663ee6fd57f0cdfd0b52d382e085
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:03:10 +0100

    Add files via upload

---
+++ Rozdzial8_zad9/Rozdzial8_zad9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rozdzial8_zad9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9. Write a program that converts a binary number to hexadecimal one.");
+
+            Console.WriteLine("Wpisz liczbe w formacie binarnym");
+
+            string bin = Console.ReadLine();
+
+            Console.WriteLine("Wynik {0}", Convert.ToInt32(bin,2).ToString("X"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
