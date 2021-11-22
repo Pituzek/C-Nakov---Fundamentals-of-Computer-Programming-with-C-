9451f5f3e3dac2f0e631b9bcd1cd109bf25d48e8
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:05:26 +0100

    Add files via upload

---
+++ Rozdzial9_zad7/Rozdzial9_zad7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Rozdzial9_zad7
+{
+    class Program
+    {
+        public static string Reverse(string number)
+        {
+            char[] charArray = number.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7. Write a method that prints the digits of a given decimal number in a reversed order. For example 256, must be printed as 652.");
+
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+
+            Console.WriteLine(Reverse(number));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial9_zad9/Rozdzial9_zad9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Rozdzial9_zad9
+{
+    class Program
+    {
+        static int ZnajdzNajwiekszaLiczbe(int[] tablica, int start, int end)
+        {
+            int maks = tablica[start];
+            int maksSave = 0;
+            int indeks = 0;
+            int tmp = 0;
+
+            for (int i = start; i < end; i++)
+            {
+
+                for (int j = i + 1; j < end; j++)
+                {
+                    if (tablica[j] > maks)
+                    {
+                        maks = tablica[j];
+                        indeks = j;
+                    }
+
+                }
+
+                tmp = tablica[i];
+                tablica[i] = tablica[indeks];
+                tablica[indeks] = tmp;
+                if (maksSave < maks) maksSave = maks;
+                maks = 0;
+
+            }
+
+            return maksSave;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9. Write a method that finds the biggest element of an array. Use that method to implement sorting in descending order.");
+
+            Console.WriteLine("\n Podaj wielkosc tablicy n");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] tab = new int[n];
+
+            for( int i=0; i<n; i++)
+            {
+                Console.WriteLine("Element {0}: ", i);
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Podaj indeks startowy");
+            int startIndeks = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj indeks koncowy");
+            int koncowyIndeks = int.Parse(Console.ReadLine());
+
+
+            int maks = ZnajdzNajwiekszaLiczbe(tab, startIndeks, koncowyIndeks);
+
+
+            Console.WriteLine("Maks liczba z tablicy to: {0}", maks);
+
+            foreach( int liczba in tab)
+            {
+                Console.Write("{0}", liczba);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
