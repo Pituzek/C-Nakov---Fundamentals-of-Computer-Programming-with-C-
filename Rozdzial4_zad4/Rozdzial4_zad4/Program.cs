7229bd536721ad781276d0af79f2f293d0b6b9fb
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 16:44:28 +0100

    Add files via upload

---
+++ Rozdzial4_zad4/Rozdzial4_zad4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rozdzial4_zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            
+            4. Write a program that prints three numbers in three virtual columns on the console.
+            Each column should have a width of 10 characters and the numbers should be left aligned. 
+            The first number should be an integer in hexadecimal; the second should be fractional positive; 
+            and the third – a negative fraction. The last two numbers have to be rounded to the second decimal place.
+
+            */
+
+            int a = 2021;
+            double b = 10.435;
+            double c = -2.434; 
+
+            Console.WriteLine(" {0,-10:X2} \n {1,-10:F2} \n {2,-10:F2} \n", a,b,c);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rozdzial4_zad7/Rozdzial4_zad7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Rozdzial4_zad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 7. Write a program that reads five integer numbers and prints their sum. If an invalid number is entered the program should prompt the user to enter another number.
+             */
+            Console.WriteLine("Podaj 5 liczb całkowitych");
+
+            int[] liczby = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Podaj {0} liczbe", i);
+                string input = Console.ReadLine();
+
+                
+                bool isInteger = int.TryParse(input, out liczby[i]);
+
+                if (isInteger == false)
+                {
+                    Console.WriteLine("Podaj inna liczbe");
+                    i--;
+                    continue;
+                }
+                else
+                {
+                    //sum += integerInput;
+
+                }
+
+            }
+
+            int integerInput = 0;
+
+            foreach (int cos in liczby)
+            {
+                integerInput += cos;
+            }
+
+            Console.WriteLine("Suma= {0}", integerInput);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
