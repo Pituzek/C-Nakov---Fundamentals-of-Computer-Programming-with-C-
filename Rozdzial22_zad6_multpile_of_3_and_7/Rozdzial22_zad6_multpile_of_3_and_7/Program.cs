2760bee1a660d63051141fd168e95ab39b0a5dd0
Pituzek <81863719+Pituzek@users.noreply.github.com>
2021-11-22 17:15:57 +0100

    Add files via upload

---
+++ Rozdzial22_zad6_multpile_of_3_and_7/Rozdzial22_zad6_multpile_of_3_and_7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Rozdzial22_zad6_multpile_of_3_and_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //6. Write a program that prints to the console all numbers from a given array (or list),
+            //that are multiples of 7 and 3 at the same time. Use the built-in extension methods
+            //with lambda expressions and then rewrite the same using a LINQ query.
+
+            List<int> listaLiczb = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21, 24, 42, 48, 63 };
+
+            var a = IEnumberableExt.GetDivisbleByThreeAndSeven(listaLiczb);
+            var b = IEnumberableExt.GetDivisibleByThreeAndSevenLinq(listaLiczb);
+
+            foreach (int x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("");
+
+            foreach (int x in b)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public static partial class IEnumberableExt
+    {
+        public static IEnumerable<T> GetDivisbleByThreeAndSeven<T>(this IEnumerable<T> collection, bool useParallel = false)
+        {
+            if (useParallel)
+            {
+                collection = collection.AsParallel();
+            }
+            return collection.Where(x => (dynamic)x % 21 == 0).OrderBy(x => x);
+        }
+
+        public static IEnumerable<T> GetDivisibleByThreeAndSevenLinq<T>(this IEnumerable<T> collection, bool useParallel = false)
+        {
+            if (useParallel)
+            {
+                collection = collection.AsParallel();
+            }
+            var test = from x in collection
+                       where (dynamic)x % 21 == 0
+                       orderby x
+                       select x;
+            return test;
+        }
+
+
+    }
+
+}
---
+++ Rozdzial22_zad7_string_extension_method/Rozdzial22_zad7_string_extension_method/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace Rozdzial22_zad7_string_extension_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7. Write an extension method for the class String that capitalizes all letters,
+            //which are the beginning of a word in a sentence in English.
+            //For example: "this iS a Sample sentence." should be converted to "This Is A Sample Sentence.".
+
+            string text = "this iS a Sample sentence.";
+
+            Console.WriteLine(StringExt.ToTitleCase(text));
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExt
+    {
+        private static bool IsLetterOrDigitExt(string s, int index)
+        {
+            return !IsAtStringBoundaries(s, index) && char.IsLetterOrDigit(s[index]);
+        }
+
+        private static bool IsWordSeparator(string s, int index)
+        {
+            if (!IsAtStringBoundaries(s, index))
+            {
+                if (s[index] == '\'' && IsLetterOrDigitExt(s, index - 1))
+                {
+                    return false;
+                }
+                else
+                {
+                    return !IsLetterOrDigitExt(s, index);
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static bool IsAtStringBoundaries(string s, int index)
+        {
+            return index == -1 || index == s.Length;
+        }
+
+        public static string ToTitleCase(this string s)
+        {
+            StringBuilder output = new StringBuilder(s.Length);
+            for (int i = 0; i < s.Length; i++)
+            {
+                output.Append(IsWordSeparator(s, i - 1) ? char.ToUpper(s[i]) : char.ToLower(s[i]));
+            }
+
+            return output.ToString();
+        }
+
+    }
+
+}
